a8ac84398a7b2766c8cdf7af42b9bf52ac303221
Wiinck <theuswinck@gmail.com>
2026-05-07 19:26:32 -0300

    Update 10_filtro_aprovados.py
    Scripts de filtragem e transformação condicional em listas

--- Lógica/10_filtro_aprovados.py
+++ Lógica/10_filtro_aprovados.py
@@ -1,7 +1,11 @@
 def main():
-    usuarios = ["Ana", "Carlos", "Beto", "Alessandra", "Edu"]
-    nomes_longos = [nomes for nomes in usuarios if len(nomes) > 5]
+    notas_turma = [4.5, 7.0, 9.0, 5.8, 10.0, 6.2]
+    notas_aprovado = [notas for notas in notas_turma if notas >= 7]
 
-    print(f"Nomes com mais de 5 letras: {nomes_longos}")
+    print(f"As notas que foram aprovadas são: {notas_aprovado}")
+
 
 main()
+
+
+
