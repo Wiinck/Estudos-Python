27ced1e8077e044db34a680f67a955d63db8df82
Wiinck <theuswinck@gmail.com>
2026-06-24 14:33:54 -0400

    feat: adiciona solucao do desafio 1049

---
+++ Lógica/Exercicios_Beecrowd/1049_Animal.py
@@ -0,0 +1,30 @@
+def main():
+    A = input().strip()
+    B = input().strip()
+    C = input().strip()
+
+    animais_dict = {
+        "vertebrado": {
+            "ave": {
+                "carnivoro": "aguia",
+                "onivoro": "pomba"
+            },
+            "mamifero": {
+                "onivoro": "homem",
+                "herbivoro": "vaca"
+            }
+        },
+        "invertebrado": {
+            "inseto": {
+                "hematofago": "pulga",
+                "herbivoro": "lagarta"
+            },
+            "anelideo": {
+                "hematofago": "sanguessuga",
+                "onivoro": "minhoca"
+            }
+        }
+    }
+
+    print(animais_dict[A][B][C])
+main()
